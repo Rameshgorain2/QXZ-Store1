ddd459f4d683415e547f289b5afbec60a1e017c8
Rameshgorain2 <kistugorain50@gmail.com>
2026-08-19 20:08:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOu3BGlFXXSYUpP0P5VLo-qSGXITptN3c",
+    authDomain: "landing-page-305c5.firebaseapp.com",
+    databaseURL: "https://landing-page-305c5-default-rtdb.firebaseio.com",
+    projectId: "landing-page-305c5",
+    storageBucket: "landing-page-305c5.firebasestorage.app",
+    messagingSenderId: "337738276300",
+    appId: "1:337738276300:web:80c5b0c289a55f7dad9169"
+};
